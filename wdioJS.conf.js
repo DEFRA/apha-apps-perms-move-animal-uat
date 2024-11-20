299d89e78e36bf71880be6d0b8e0e695dd422cfa
Eoin Corr <eoin.corr@dovehousesolutions.com>
2024-11-20 14:11:16 +0000

    fix

--- wdioJS.conf.js
+++ wdioJS.conf.js
@@ -2,7 +2,7 @@ const merge = require('deepmerge')
 const wdioConf = require('./wdio.conf')
 
 exports.config = merge(wdioConf.config, {
-  specs: ['./test/specs/**.*.js'],
+  specs: ['./test/specs/**/*.js'],
   exclude: ['./test/specs/noJavascript/**/*.js'],
   capabilities: [
     {
